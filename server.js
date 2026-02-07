998efbf9baa4b53d108c9e255760d29286702dc5
DapszSigma <dapsz1959@gmail.com>
2026-02-07 14:45:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ import tiktok from "./api/tiktok.js";
 
 const app = express();
 
-app.get("/api/tiktok", (req, res) => {
-  return tiktok(req, res);
-});
+app.get("/api/tiktok", tiktok);
 
 const PORT = 3000;
 
